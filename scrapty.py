3c5a4f288283d9d0f9b59706e2db1ad95313c290
Mohsen Walton <waltonmohsen@gmail.com>
2020-02-25 18:42:32 +0330

    init

---
+++ scrapty.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.firefox.options import Options
+import datetime
+import time
+import requests
+import random
+import os
+from os import path
+
+from selenium.webdriver.support.wait import WebDriverWait
+
+url = "https://www.reddit.com/r/memes/"  # url for scrap
+scrollPauseTime = 15  # scroll Pause time
+t = datetime.datetime.now()
+folderPath = "memes-" + str(t.year) + "-" + str(t.month) + "-" + str(t.day) + "-" + str(t.hour) + "-" + str(
+    t.minute) + "-" + str(
+    t.second)  # generate folder path for saving images each time script run
+images_list = []  # local image list
+images_url = []  # external image list
+
+
+def dl_reddit_memes(step, scrollheight, headless):
+    """
+     @param step:step to scroll default : 5000
+     @param scrollheight: scroll height of page
+     @param headless: open driver GUI (True or false)
+    """
+    options = Options()
+    # change to false if you want to see firefox
+    options.headless = headless
+    counter = 5000
+    # create new browser
+    driver = webdriver.Firefox(options=options)
+    try:
+        print("loading page (it depeneds on your internet speed) ... please wait")
+        # open the URL
+        driver.get(url)
+        # scroll the page
+        while counter <= scrollheight:
+            # excute javascript that scroll the page
+            driver.execute_script("window.scrollTo(0," + str(counter) + ");")
+            counter += step
+            # time.sleep(1)
+        time.sleep(scrollPauseTime)
+        # check for path to not exists
+        if not path.exists(folderPath):
+            # make new directory for images
+            os.mkdir(folderPath)
+            # loop through image src attributes
+            for i in driver.find_elements_by_class_name('ImageBox-image'):
+                # check image source if faild to get
+                if i.get_attribute('src') != "None" or i.get_attribute('src') != "none":
+                    # get the memes image url and append it to images_url list
+                    images_url.append(i.get_attribute('src'))
+                    # get image src binary
+                    img = requests.get(i.get_attribute('src')).content
+                    # save each image into it file with random number
+                    with open(imgName := folderPath + "/" + "meme-" + str(random.randrange(5000)) + ".jpg", "wb") as f:
+                        f.write(img)
+                        print(imgName + " downloaded into " + folderPath)
+                        # append image local name to images_list
+                        images_list.append(imgName)
+    # get the error on close the driver
+    except StaleElementReferenceException:
+        print(StaleElementReferenceException.msg)
+        driver.close()
+    print("all memes downloaded succefully!")
+    driver.close()
+
---
+++ telegram_bot.py
@@ -0,0 +1,45 @@
+import logging
+from telegram.ext import Updater
+from scrapty import *
+from telegram.ext import CommandHandler
+
+# log any errors
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+# telegram token
+botToken = "998044371:AAFpfhXXr8j-V3C8gK5nfTXghws7Z7SoBmE"
+
+# logging.basicConfig(level=logging.DEBUG,
+#                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+updater = Updater(token=botToken, use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    """
+     start bot command handler
+    """
+    context.bot.send_message(chat_id="@toplessmemes",
+                             text="Hello , its a private Bot ! not working for every one")
+
+
+def send(update, context):
+    # call the scrapper
+    dl_reddit_memes(5000, 100000, True)
+    for image in images_url:
+        time.sleep(4200)
+        context.bot.send_photo(chat_id="@toplessmemes", photo=image,
+                               caption="Join top memes: https://t.me/toplessmemes \n")
+
+
+# get user /start command
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+# get send /send command
+send_handler = CommandHandler('send', send)
+dispatcher.add_handler(send_handler)
+
+# start the bot
+updater.start_polling()
